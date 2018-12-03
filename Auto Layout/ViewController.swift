3930a4f4ab4226d3f0dbc1bd21854d0c997a8016
Adriano Kaito <adrianokaito@Adrianos-MacBook-Pro.local>
2018-12-03 20:35:57 -0200

    first commit

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by Adriano Kaito on 26/11/18.
+//  Copyright © 2018 AK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
